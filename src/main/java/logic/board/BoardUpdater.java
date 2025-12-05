b615a940a40f49a08e0ec9a50c27067e9daa8f86
tanravikorn <tanravikorn@gmail.com>
2025-12-06 02:00:55 +0700

    package board has done

--- src/main/java/logic/board/BoardUpdater.java
+++ src/main/java/logic/board/BoardUpdater.java
@@ -1,4 +1,77 @@
 package logic.board;
 
+import logic.candy.Candy;
+import logic.candy.CandyColor;
+import logic.utils.Point;
+
+import java.util.*;
+
 public class BoardUpdater {
+
+    private static final Random random = new Random();
+    public static void updateBoard(Board board, Set<Point> markRemoves){
+        for(Point p : markRemoves){
+            if(board.isValid(p.r,p.c)){
+                Candy c = board.getCandy(p.r,p.c);
+                if(c != null){
+                    c.setIsRemove(true);
+                }
+                board.setCandy(p.r, p.c,null);
+            }
+        }
+
+        BoardPhysics.applyGravity(board);
+
+        refillBoard(board);
+    }
+
+    private static void refillBoard(Board board){
+        int rows = board.getRows();
+        int cols = board.getCols();
+        for(int c =0;c <cols;c++){
+            for(int r =0;r <rows;r++){
+                if(board.getCandy(r,c) == null){
+                    Candy newCandy = generateRandomCandy(r,c);
+                    board.setCandy(r,c,newCandy);
+                }
+            }
+        }
+    }
+    private static Candy generateRandomCandy(int r, int c){
+        CandyColor[] colors = {
+                CandyColor.PURPLE,
+                CandyColor.YELLOW,
+                CandyColor.RED,
+                CandyColor.BLUE,
+                CandyColor.GREEN
+        };
+        CandyColor randomCandy = colors[random.nextInt(colors.length)];
+        return new Candy(r,c, randomCandy);
+    }
+
+    public static void shuffleBoard(Board board){
+        List<Candy> allCandies = new ArrayList<>();
+        int rows = board.getRows();
+        int cols = board.getCols();
+
+        for(int r= 0;r<rows;r++){
+            for(int c =0;c <cols;c++){
+                Candy candy = board.getCandy(r,c);
+                if(candy != null){
+                    allCandies.add(candy);
+                }
+            }
+        }
+        Collections.shuffle(allCandies);
+        int index = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (index < allCandies.size()) {
+                    Candy shuffledCandy = allCandies.get(index++);
+                    board.setCandy(r, c, shuffledCandy);
+                }
+            }
+        }
+    }
 }
--- src/main/java/logic/utils/MatchFinder.java
+++ src/main/java/logic/utils/MatchFinder.java
@@ -3,6 +3,7 @@ package logic.utils;
 import logic.board.Board;
 import logic.candy.Candy;
 import logic.candy.CandyColor;
+import logic.candy.CandyType;
 
 import java.util.*;
 
@@ -65,7 +66,7 @@ public class MatchFinder {
                     }
                     currentRun.clear();
                 }
-                if(current != null && current.getColor() != CandyColor.NONE){
+                if(current != null && current.getColor() != CandyColor.NONE && !current.isRemove()){
                     currentRun.add(current);
                 }
             }
@@ -84,7 +85,7 @@ public class MatchFinder {
                     }
                     currentRun.clear();
                 }
-                if(current != null && current.getColor() != CandyColor.NONE){
+                if(current != null && current.getColor() != CandyColor.NONE && !current.isRemove()){
                     currentRun.add(current);
                 }
             }
@@ -120,14 +121,46 @@ public class MatchFinder {
 
     public boolean checkColor(int r, int c, CandyColor color){
         Candy candy = board.getCandy(r,c);
-        return (candy != null) && (color == candy.getColor());
+        return (candy != null) && (color == candy.getColor()) && !candy.isRemove();
     }
 
     public boolean shouldEndRun(List<Candy> run, Candy current){
         if(run.isEmpty()) return false;
         if(current == null) return true;
-        if(current.getColor() == CandyColor.NONE) return true;
+        if(current.getColor() == CandyColor.NONE || current.isRemove()) return true;
         Candy last = run.get(run.size()-1);
         return last.getColor() != current.getColor();
+    }
+
+    public boolean hasPotentialMatch(){
+        int rows = board.getRows();
+        int cols = board.getCols();
+
+        for(int r =0;r < rows;r++){
+            for(int c =0;c < cols;c++){
+                if(c < cols - 1){
+                    if(checkVirtualSwap(r,c,r,c+1)) return true;
+                }
+                if(r < rows-1){
+                    if(checkVirtualSwap(r,c,r+1,c)) return true;
+                }
+            }
+        }
+         return false;
+    }
+    private boolean checkVirtualSwap(int r1, int c1, int r2, int c2){
+        Candy candy1 = board.getCandy(r1,c1);
+        Candy candy2 = board.getCandy(r2,c2);
+        if(candy1 == null || candy2 == null) return false;
+
+        if(candy1.getType() == CandyType.COLOR_BOMB || candy2.getType() == CandyType.COLOR_BOMB) return true;
+
+
+        board.setCandy(r1,c1,candy2);
+        board.setCandy(r2,c2,candy1);
+        boolean hasMatch = !findMatchAt(r1, c1).isEmpty() || !findMatchAt(r2, c2).isEmpty();
+        board.setCandy(r1,c1,candy1);
+        board.setCandy(r2,c2,candy2);
+        return hasMatch;
     }
 }

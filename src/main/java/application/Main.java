f8d2554199a54dddd9b01b9e647928cf36657e2a
tanravikorn <tanravikorn@gmail.com>
2025-12-10 02:52:21 +0700

    final77777

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -4,7 +4,7 @@ import gui.base.ViewManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-// เปลี่ยนชื่อคลาสให้ตรงกับชื่อไฟล์ (Main.java)
+
 public class Main extends Application {
 
     @Override
